f27fdd354c1c0ad8c2efea2faa3d86b7afdf298c
ashrafulislampro <ashrafulislam1659@gmail.com>
2023-09-11 18:07:37 +0600

    refine bitwise operation problem solving

--- bitWiseOperators.js
+++ bitWiseOperators.js
@@ -1,17 +1,21 @@
 function getMaxLessThanK(n, k){
     let max = 0;
+   
     for(let i = 0; i <= n; i++){
         for(let j = i + 1; j <= n; j++){
+
             let bitAnd = i & j;
-            if(max < bitAnd && bitAnd < k){
-                max = bitAnd;
+            console.log("bitAnd7", bitAnd, "index", i, j); 
+            if(max < bitAnd && bitAnd < k){ 
+                max = bitAnd; 
             }
         }
         
     }
+    console.log("max", max);
     return max;
 }
 
 
-const result = getMaxLessThanK(2, 2);
-console.log("result", result);
+const result = getMaxLessThanK(5, 4);
+// console.log("result", result);
